f70f2eb4b7d86a62ca01bebe55c157ba6bfd73f1
Hauke Klement <dev@hklement.de>
2015-01-29 15:08:11 +0100

    minor change

--- spec/features/factories_spec.rb
+++ spec/features/factories_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe 'Factories' do
-  it 'are all valid', permitted_execution_time: 30 do
+  it 'are all valid', docker: true, permitted_execution_time: 30 do
     expect { FactoryGirl.lint }.not_to raise_error
   end
 end
