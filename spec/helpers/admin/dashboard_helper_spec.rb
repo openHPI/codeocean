3f12e25b7f6f5ee0117f7ba63b90723a28fd5796
Hauke Klement <dev@hklement.de>
2015-02-24 16:01:26 +0100

    added tests

---
+++ spec/helpers/admin/dashboard_helper_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+describe Admin::DashboardHelper do
+  describe '#dashboard_data' do
+    it 'includes Docker-related data' do
+      expect(dashboard_data).to include(:docker)
+    end
+  end
+
+  describe '#docker_data' do
+    before(:each) { FactoryGirl.create(:ruby) }
+
+    it 'contains an entry for every execution environment' do
+      expect(docker_data.length).to eq(ExecutionEnvironment.count)
+    end
+
+    it 'contains the pool size for every execution environment' do
+      expect(docker_data.first.symbolize_keys).to include(:pool_size)
+    end
+
+    it 'contains the number of available containers for every execution environment' do
+      expect(DockerContainerPool).to receive(:quantities).exactly(ExecutionEnvironment.count).times.and_call_original
+      expect(docker_data.first).to include(:quantity)
+    end
+  end
+end
