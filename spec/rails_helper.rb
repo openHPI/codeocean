fdb9243d2143d5d1497b0b0fa1af8c9f4593f02e
Hauke Klement <dev@hklement.de>
2015-02-24 09:58:48 +0100

    don't use ActiveRecord fixtures

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -19,9 +19,6 @@ Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
   config.include(Authentication, type: :feature)
   config.include(WaitForAjax, type: :feature)
   config.include(Sorcery::TestHelpers::Rails::Controller, type: :controller)
