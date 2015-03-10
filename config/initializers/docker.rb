c6e97cf00e7d3b7f28d859e6c486b7429d86d54f
Hauke Klement <dev@hklement.de>
2015-03-10 08:59:11 +0100

    don't always populate the Docker container pool when loading the environment but only when starting the server

--- config/initializers/docker.rb
+++ config/initializers/docker.rb
@@ -1,6 +1,6 @@
 DockerClient.initialize_environment unless Rails.env.test? && `which docker`.blank?
 
-if ActiveRecord::Base.connection.tables.present? && DockerContainerPool.config[:active]
+if defined?(Rails::Server) && ActiveRecord::Base.connection.tables.present? && DockerContainerPool.config[:active]
   DockerContainerPool.start_refill_task
   at_exit { DockerContainerPool.clean_up }
 end
