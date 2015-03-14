ebc06c09dbc7f81ea142b50cf04d0f65d48506b5
Hauke Klement <dev@hklement.de>
2015-03-14 12:25:53 +0100

    moved method

--- app/assets/javascripts/editor.js
+++ app/assets/javascripts/editor.js
@@ -212,6 +212,30 @@ $(function() {
     showTab(3);
   };
 
+  var handleStderrOutputForFlowr = function(event) {
+    var flowrUrl = $('#flowrHint').data('url');
+    var json = JSON.parse(event.data);
+    var stderrOutput = '';
+
+    if (json.stderr) {
+      stderrOutput += json.stderr;
+    } else if (json.code) {
+      var flowrHintBody = $('#flowrHint .panel-body');
+
+      $.getJSON(flowrUrl + '&query=' + escape(stderrOutput), function(data) {
+        _.each(_.compact(data.queryResults), function(question, index) {
+          var collapsibleTileHtml = flowrResultHtml.replace(/{{collapseId}}/g, 'collapse-' + index).replace(/{{headingId}}/g, 'heading-' + index);
+          var resultTile = $(collapsibleTileHtml);
+          resultTile.find('h4 > a').text(question.title);
+          resultTile.find('.panel-body').append(question.body);
+          flowrHintBody.append(resultTile);
+        });
+
+        $('#flowrHint').fadeIn();
+      });
+    }
+  };
+
   var handleTestResponse = function(response) {
     clearOutput();
     printOutput(response[0], false, 0);
@@ -616,28 +640,4 @@ $(function() {
     showFirstFile();
     showRequestedTab();
   }
-
-  var handleStderrOutputForFlowr = function(event) {
-    var flowrUrl = $('#flowrHint').data('url');
-    var json = JSON.parse(event.data);
-    var stderrOutput = '';
-
-    if (json.stderr) {
-      stderrOutput += json.stderr;
-    } else if (json.code) {
-      var flowrHintBody = $('#flowrHint .panel-body');
-
-      $.getJSON(flowrUrl + '&query=' + escape(stderrOutput), function(data) {
-        _.each(_.compact(data.queryResults), function(question, index) {
-          var collapsibleTileHtml = flowrResultHtml.replace(/{{collapseId}}/g, 'collapse-' + index).replace(/{{headingId}}/g, 'heading-' + index);
-          var resultTile = $(collapsibleTileHtml);
-          resultTile.find('h4 > a').text(question.title);
-          resultTile.find('.panel-body').append(question.body);
-          flowrHintBody.append(resultTile);
-        });
-
-        $('#flowrHint').fadeIn();
-      });
-    }
-  };
 });
