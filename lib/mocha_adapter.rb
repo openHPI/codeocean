35b2177c7194ebaa915675987e7dfcdc97fc252e
Jan Renz <jan.renz@gmail.com>
2015-06-17 15:29:35 +0200

    Add mocha adpater

---
+++ lib/mocha_adapter.rb
@@ -0,0 +1,16 @@
+class MochaAdapter < TestingFrameworkAdapter
+  SUCCESS_REGEXP = /(\d+) passing/
+  FAILURES_REGEXP = /(\d+) failing/
+
+  def self.framework_name
+    'Mocha'
+  end
+
+  def parse_output(output)
+    matches_success = SUCCESS_REGEXP.match(output[:stderr])
+    matches_failed = FAILURES_REGEXP.match(output[:stderr])
+    failed = matches_failed ? matches_failed.captures.try(:first).to_i : 0
+    success = matches_success ? matches_success.captures.try(:first).to_i : 0
+    {count: success+failed, failed: failed}
+  end
+end
