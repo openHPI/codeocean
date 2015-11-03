d0ba0a970bdb00cec01e48146c11df26d7bc56ae
Ralf Teusner <ralf.teusner@hpi.de>
2015-11-03 16:12:23 +0100

    whitelist localhost on development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.web_console.whitelisted_ips = '192.168.0.0/16'
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
