baf33419cf3c6153979ec5cfe7c3670f3421fc3d
Maximilian Grundke <maximiliangrundke@googlemail.com>
2015-11-26 16:25:59 +0100

    Update editor content on submission selection

---
+++ app/assets/javascripts/submission_statistics.js
@@ -0,0 +1,32 @@
+$(function() {
+
+  if ($.isController('exercises') && $('#timeline').isPresent()) {
+
+    var editors = $('.editor');
+    var slider = $('#slider>input');
+    var submissions = $('#data').data('submissions');
+    var files = $('#data').data('files');
+
+    editors.each(function(index, editor) {
+      currentEditor = ace.edit(editor);
+      currentEditor.$blockScrolling = Infinity;
+      currentEditor.setReadOnly(true);
+    });
+
+    slider.on('change', function(event) {
+      var currentSubmission = slider.val();
+      var currentFiles = JSON.parse(files[currentSubmission]);
+
+      editors.each(function(index, editor) {
+        currentEditor = ace.edit(editor);
+        fileContent = "";
+        if (currentFiles[index]) {
+          fileContent = currentFiles[index].content
+        }
+        currentEditor.getSession().setValue(fileContent);
+      });
+    });
+
+  }
+
+});
