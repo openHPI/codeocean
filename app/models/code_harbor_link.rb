5a190c0c792185d509d08badc6af41f6a4cd9be7
leo.selig <dev.selig@gmail.com>
2016-02-12 10:57:44 +0100

    Fix wrong belongs_to :user
    - because of polymorphic user model this didn't work
    - changed to :internal_user because that's anyway the only type of user we
      want to impoprt from
    
    (leoselig/codeocean#1)

--- app/models/code_harbor_link.rb
+++ app/models/code_harbor_link.rb
@@ -2,7 +2,9 @@ class CodeHarborLink < ActiveRecord::Base
   validates :oauth2token, presence: true
   validates :user_id, presence: true
 
-  belongs_to :user
+  belongs_to :internal_user, foreign_key: :user_id
+  alias_method :user, :internal_user
+  alias_method :user=, :internal_user=
 
   def to_s
     oauth2token
