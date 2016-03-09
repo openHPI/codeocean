dcb05b9169160d622fc16bbc909284052d04496b
Maximilian Grundke <maximiliangrundke@googlemail.com>
2016-03-09 16:21:32 +0100

    Fix testrun result for docker container depletion

--- app/controllers/concerns/submission_scoring.rb
+++ app/controllers/concerns/submission_scoring.rb
@@ -7,7 +7,9 @@ module SubmissionScoring
         assessor = Assessor.new(execution_environment: submission.execution_environment)
         output = execute_test_file(file, submission)
         assessment = assessor.assess(output)
-        Testrun.new(submission: submission, file: file, passed: assessment[:passed] == assessment[:count], output: output[:stderr]).save
+        passed = ((assessment[:passed] == assessment[:count]) and (assessment[:score] > 0))
+        testrun_output = passed ? nil : output[:stderr]
+        Testrun.new(submission: submission, file: file, passed: passed, output: testrun_output).save
         output.merge!(assessment)
         output.merge!(filename: file.name_with_extension, message: feedback_message(file, output[:score]), weight: file.weight)
       end
