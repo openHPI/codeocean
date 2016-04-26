07fa0ef2a9077e1bfb5a778bd6b841ff3da8c7dc
Maximilian Grundke <maximiliangrundke@googlemail.com>
2016-04-26 13:58:49 +0200

    Add question to RequestForComment model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160302133540) do
+ActiveRecord::Schema.define(version: 20160426114951) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -190,6 +190,7 @@ ActiveRecord::Schema.define(version: 20160302133540) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "user_type"
+    t.text     "question"
   end
 
   create_table "submissions", force: true do |t|
