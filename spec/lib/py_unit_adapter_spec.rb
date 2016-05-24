ef441930a4c5e336acbcfb8cba8efc1a04d79422
yqbk <jksyrek@gmail.com>
2016-05-24 16:32:15 +0200

    Fix in py_unit_adapter_spec

--- spec/lib/py_unit_adapter_spec.rb
+++ spec/lib/py_unit_adapter_spec.rb
@@ -4,7 +4,7 @@ describe PyUnitAdapter do
   let(:adapter) { described_class.new }
   let(:count) { 42 }
   let(:failed) { 25 }
-  let(:error_matches) { [] } }
+  let(:error_matches) { [] }
   let(:stderr) { "Ran #{count} tests in 0.1s\n\nFAILED (failures=#{failed})" }
 
   describe '#parse_output' do
