8d030e42e9621391c4c9aa4a7de32eb731cdba27
Maximilian Grundke <maximiliangrundke@googlemail.com>
2016-06-10 18:08:57 +0200

    Allow templates to include the file name as a macro

--- app/controllers/code_ocean/files_controller.rb
+++ app/controllers/code_ocean/files_controller.rb
@@ -11,7 +11,9 @@ module CodeOcean
     def create
       @file = CodeOcean::File.new(file_params)
       if @file.file_template_id
-        @file.content = FileTemplate.find(@file.file_template_id).content
+        content = FileTemplate.find(@file.file_template_id).content
+        content.sub! '{{file_name}}', @file.name
+        @file.content = content
       end
       authorize!
       create_and_respond(object: @file, path: proc { implement_exercise_path(@file.context.exercise, tab: 2) })
