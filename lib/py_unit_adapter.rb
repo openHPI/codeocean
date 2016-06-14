74b33492cb58a25c2efca4fff1f4a9c52c14dd4c
Ralf Teusner <ralf.teusner@hpi.de>
2016-06-14 10:42:15 +0200

    fixed python failure regex.

--- lib/py_unit_adapter.rb
+++ lib/py_unit_adapter.rb
@@ -1,6 +1,6 @@
 class PyUnitAdapter < TestingFrameworkAdapter
   COUNT_REGEXP = /Ran (\d+) test/
-  FAILURES_REGEXP = /FAILED \((failures)|(errors)=(\d+)\)/
+  FAILURES_REGEXP = /FAILED \((failures|errors)=(\d+)\)/
   ASSERTION_ERROR_REGEXP = /AssertionError:\s(.*)/
 
   def self.framework_name
@@ -10,7 +10,7 @@ class PyUnitAdapter < TestingFrameworkAdapter
   def parse_output(output)
     count = COUNT_REGEXP.match(output[:stderr]).captures.first.to_i
     matches = FAILURES_REGEXP.match(output[:stderr])
-    failed = matches ? matches.captures.try(:third).to_i : 0
+    failed = matches ? matches.captures.try(:second).to_i : 0
     error_matches = ASSERTION_ERROR_REGEXP.match(output[:stderr]).try(:captures) || []
     {count: count, failed: failed, error_messages: error_matches}
   end
