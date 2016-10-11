7a76c4c6f15aca9d3144e2abe79598be9ae65546
yqbk <jksyrek@gmail.com>
2016-10-11 13:12:00 +0200

    try out new code

--- spec/lib/docker_client_spec.rb
+++ spec/lib/docker_client_spec.rb
@@ -26,6 +26,10 @@ describe DockerClient, docker: true do
   end
 
   describe '.container_creation_options' do
+    it "is TODO" do
+      pending("heavy load on the docker daemon")
+    end
+
     let(:container_creation_options) { described_class.container_creation_options(execution_environment) }
 
     it 'specifies the Docker image' do
@@ -159,6 +163,9 @@ describe DockerClient, docker: true do
   end
 
   describe '.destroy_container' do
+    it "is TODO" do
+      pending("heavy load on the docker daemon")
+    end
     let(:container) { described_class.create_container(execution_environment) }
     after(:each) { described_class.destroy_container(container) }
 
