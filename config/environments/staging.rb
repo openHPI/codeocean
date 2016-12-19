f82737a7d4702968cfb41ab426c42888b52dc9f9
Ralf Teusner <ralf.teusner@hpi.de>
2016-12-19 15:08:51 +0100

    some small changes to staging environment (no caching and eager loading, some more errors).

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,14 +1,17 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  # Code is not reloaded between requests.
-  config.cache_classes = true
+  # true: Code is not reloaded between requests.
+  # false:   In the development environment your application's code is reloaded on
+  #          every request. This slows down response time but is perfect for development
+  #          since you don't have to restart the web server when you make code changes.
+  config.cache_classes = false
 
-  # Eager load code on boot. This eager loads most of Rails and
-  # your application in memory, allowing both threaded web servers
-  # and those relying on copy on write to perform better.
-  # Rake tasks automatically ignore this option for performance.
-  config.eager_load = true
+  # true: Eager load code on boot. This eager loads most of Rails and
+  #       your application in memory, allowing both threaded web servers
+  #       and those relying on copy on write to perform better.
+  #       Rake tasks automatically ignore this option for performance.
+  config.eager_load = false
 
   #enable web console in staging
   config.web_console.development_only = false
@@ -16,6 +19,17 @@ Rails.application.configure do
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
+
+  # Raise an error on page load if there are pending migrations.
+  config.active_record.migration_error = :page_load
+
+  # Adds additional error checking when serving assets at runtime.
+  # Checks for improperly declared sprockets dependencies.
+  # Raises helpful error messages.
+  config.assets.raise_runtime_errors = true
+
+  # Raise errors for missing translations.
+  config.action_view.raise_on_missing_translations = true
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
