8b4e312a43be0f1b663bcedb22e8cf05003fab3b
Niklas Kiefer <niklas.kiefer@student.hpi.de>
2017-01-07 11:42:53 +0100

    WIP

--- app/controllers/remote_evaluation_controller.rb
+++ app/controllers/remote_evaluation_controller.rb
@@ -5,14 +5,16 @@ class RemoteEvaluationController < ApplicationController
 
   # POST /remote_evaluation/evaluate
   # @param token
-  # @exercise_files
+  # @param file_attributes
   def evaluate
     token = params[:token]
+    files_attributes = params[:files_attributes] || []
     # todo: im Model nachgucken, ob es zu diesem token einen user und eine exerciseId gibt
     # wenn ja ausführen
-    ## submission erstellen (submission create) mit cause "remoteAssess", fileAttributes = exerciseFiles: { Array of {name: Dateiname, content: Inhalt der Datei} } und exercise_id
+    ## submission erstellen (submission create) mit cause "remoteAssess", fileAttributes: { Array of {name: Dateiname, content: Inhalt der Datei} } und exercise_id
+    submission = Submission.new(:cause => 'remoteAssess', :files_attributes => files_attributes)
+    render json: submission
     ## extra: validiere, ob files wirklich zur Übung gehören (wenn allowNewFiles-flag nicht gesetzt ist)
     # wenn nein und token expired (nach einem Monat): sende antwort, dass token expired und: Log dich ein, suche Übung und update das token
-    render :nothing => true
   end
 end
