15de671425191a829c1c46aa7735d849f206ee1a
Niklas Kiefer <niklas.kiefer@student.hpi.de>
2017-01-11 22:18:02 +0100

    some improvements

--- app/controllers/remote_evaluation_controller.rb
+++ app/controllers/remote_evaluation_controller.rb
@@ -1,28 +1,32 @@
 class RemoteEvaluationController < ApplicationController
+  include RemoteEvaluationParameters
 
   skip_after_action :verify_authorized
   skip_before_action :verify_authenticity_token
 
   # POST /evaluate
   # @param validation_token
-  # @param file_attributes
+  # @param files_attributes
   def evaluate
-    validation_token = params[:validation_token]
-    files_attributes = params[:files_attributes] || []
+
+    puts params
+
+    validation_token = remote_evaluation_params[:validation_token]
+    files_attributes = remote_evaluation_params[:files_attributes] || []
 
     # todo extra: validiere, ob files wirklich zur Übung gehören (wenn allowNewFiles-flag nicht gesetzt ist)
     if (remote_evaluation_mapping = RemoteEvaluationMapping.find_by(:validation_token => validation_token))
       puts remote_evaluation_mapping.exercise_id
       puts remote_evaluation_mapping.user_id
 
-      ## submission erstellen (submission create) mit cause "remoteAssess", fileAttributes: { Array of {name: Dateiname, content: Inhalt der Datei} } und exercise_id
+      ## submission erstellen (submission create) mit cause "remoteAssess", file_attributes: { Array of {name: Dateiname, content: Inhalt der Datei} } und exercise_id
       # todo: create instead of new to save in db!
-      submission = Submission.new(
-          :cause => 'remoteAssess',
-          :files_attributes => files_attributes,
-          :exercise_id => remote_evaluation_mapping.exercise_id,
-          :user_id => remote_evaluation_mapping.user_id)
-      render json: submission
+      @submission = Submission.new(remote_evaluation_params.except(:validation_token))
+      @submission.exercise_id = remote_evaluation_mapping.exercise_id
+      @submission.user_id = remote_evaluation_mapping.user_id
+      @submission.cause = "remoteAssess"
+      @submission.save
+      render json: @submission
     else
       # todo: better output
       # todo: check token expired?
