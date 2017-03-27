65256cdfd91defd4ad198d771f25f9b58a38c7c3
Ralf Teusner <ralf.teusner@hpi.de>
2017-03-27 09:50:10 +0200

    change intervention groups also to 20 20 60

--- lib/user_group_separator.rb
+++ lib/user_group_separator.rb
@@ -1,13 +1,13 @@
 class UserGroupSeparator
 
-  # seperates user into 30% no intervention, 30% break intervention, 40% rfc intervention
+  # seperates user into 20% no intervention, 20% break intervention, 60% rfc intervention
   def self.getInterventionGroup(user)
     lastDigitId = user.id % 10
-    if lastDigitId < 3 # 0,1,2
+    if lastDigitId < 2 # 0,1
       :no_intervention
-    elsif lastDigitId < 6 # 3,4,5
+    elsif lastDigitId < 4 # 2,3
       :break_intervention
-    else # 6,7,8,9
+    else # 4,5,6,7,8,9
       :rfc_intervention
     end
   end
