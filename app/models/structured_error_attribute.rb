4d684a7a05e24911abbab4ca5468b97be96738ba
Maximilian Grundke <maximiliangrundke@googlemail.com>
2017-07-12 09:52:33 +0200

    Extract structured errors on run and submit

--- app/models/structured_error_attribute.rb
+++ app/models/structured_error_attribute.rb
@@ -1,4 +1,9 @@
 class StructuredErrorAttribute < ActiveRecord::Base
   belongs_to :structured_error
   belongs_to :error_template_attribute
+
+  def self.create_from_template(attribute, structured_error, message_buffer)
+    value = message_buffer.match(attribute.regex).captures[0]
+    self.create(structured_error: structured_error, error_template_attribute: attribute, value: value)
+  end
 end
