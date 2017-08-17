f30b60657f75fc44863f35b0b2a5a2bb2baf5bba
Maximilian Grundke <maximiliangrundke@googlemail.com>
2017-08-17 13:56:28 +0200

    Get more comment information from the server

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,7 @@
 class Comment < ActiveRecord::Base
   # inherit the creation module: encapsulates that this is a polymorphic user, offers some aliases and makes sure that all necessary attributes are set.
   include Creation
-  attr_accessor :username
+  attr_accessor :username, :date, :updated
 
   belongs_to :file, class_name: 'CodeOcean::File'
   belongs_to :user, polymorphic: true
