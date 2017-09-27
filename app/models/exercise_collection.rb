f0c0621b3114d17ffd7d54f959fb53d866c6d628
Maximilian Grundke <maximiliangrundke@googlemail.com>
2017-09-27 16:52:42 +0200

    Implement show route

--- app/models/exercise_collection.rb
+++ app/models/exercise_collection.rb
@@ -2,4 +2,8 @@ class ExerciseCollection < ActiveRecord::Base
 
   has_and_belongs_to_many :exercises
 
+  def to_s
+    "#{I18n.t('activerecord.models.exercise_collection.one')}: #{name} (#{id})"
+  end
+
 end
