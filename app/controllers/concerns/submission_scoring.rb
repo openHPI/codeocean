e52c9213a1515eed2258dfe9b381267fb9ed1d83
Ralf Teusner <ralf.teusner@hpi.de>
2017-10-04 16:42:42 +0200

    try to activate interpretation of carriage return

--- app/controllers/concerns/submission_scoring.rb
+++ app/controllers/concerns/submission_scoring.rb
@@ -8,7 +8,7 @@ module SubmissionScoring
         output = execute_test_file(file, submission)
         assessment = assessor.assess(output)
         passed = ((assessment[:passed] == assessment[:count]) and (assessment[:score] > 0))
-        testrun_output = passed ? nil : 'message: ' + output[:message].to_s + '\n stdout: ' + output[:stdout].to_s + '\n stderr: ' + output[:stderr].to_s
+        testrun_output = passed ? nil : 'message: ' + output[:message].to_s + "\n stdout: " + output[:stdout].to_s + "\n stderr: " + output[:stderr].to_s
         Testrun.new(submission: submission, cause: 'assess', file: file, passed: passed, output: testrun_output).save
         output.merge!(assessment)
         output.merge!(filename: file.name_with_extension, message: feedback_message(file, output[:score]), weight: file.weight)
