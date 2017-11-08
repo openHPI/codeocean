2118e610f91e510edf7749943684ffe30d6a1fd1
Ralf Teusner <ralf.teusner@hpi.de>
2017-11-08 17:06:19 +0100

    fixed broken migration

--- db/migrate/20160204111716_add_user_to_code_harbor_link.rb
+++ db/migrate/20160204111716_add_user_to_code_harbor_link.rb
@@ -1,5 +1,5 @@
 class AddUserToCodeHarborLink < ActiveRecord::Migration
   def change
-    add_reference :code_harbor_links, :user, index: true, foreign_key: true
+    add_reference :code_harbor_links, :user,  polymorphic: true, index: true
   end
 end
