65a6e390a54d1c1e2fdef3298c7bbc452cccb26f
Ralf Teusner <ralf.teusner@hpi.de>
2018-01-31 13:02:38 +0100

    fix migration

--- db/migrate/20180130172021_add_reached_full_score_to_request_for_comment.rb
+++ db/migrate/20180130172021_add_reached_full_score_to_request_for_comment.rb
@@ -2,7 +2,7 @@ class AddReachedFullScoreToRequestForComment < ActiveRecord::Migration
   def up
     add_column :request_for_comments, :full_score_reached, :boolean, default: false
     RequestForComment.all.each { |rfc|
-      if (rfc.submission.exercise.has_user_solved(rfc.user))
+      if (rfc.submission.present? && rfc.submission.exercise.has_user_solved(rfc.user))
         rfc.full_score_reached = true
         rfc.save
       end
