b5f282b20634ecebacdfbbc5768d784142d1da4b
Maximilian Grundke <maximiliangrundke@googlemail.com>
2018-01-31 11:16:41 +0100

    Correctly set environment

--- config/schedule.rb
+++ config/schedule.rb
@@ -20,6 +20,7 @@
 # Learn more: http://github.com/javan/whenever
 
 set :output, Whenever.path + '/log/whenever_$(date +%Y%m%d%H%M%S).log'
+set :environment, ENV['RAILS_ENV'] if ENV['RAILS_ENV']
 
 every 1.day, at: '3:00 am' do
   rake 'detect_exercise_anomalies:with_at_least[50,50]'
