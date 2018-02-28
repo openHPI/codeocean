897950e65842085371e2e9ca0f3a1e5ffee259ba
Maximilian Grundke <maximiliangrundke@googlemail.com>
2018-02-28 10:36:12 +0100

    Migrate in batches of 1000 records

--- db/migrate/20180130172021_add_reached_full_score_to_request_for_comment.rb
+++ db/migrate/20180130172021_add_reached_full_score_to_request_for_comment.rb
@@ -1,8 +1,8 @@
 class AddReachedFullScoreToRequestForComment < ActiveRecord::Migration
   def up
     add_column :request_for_comments, :full_score_reached, :boolean, default: false
-    RequestForComment.all.each { |rfc|
-      if (rfc.submission.present? && rfc.submission.exercise.has_user_solved(rfc.user))
+    RequestForComment.find_each { |rfc|
+      if rfc.submission.present? and rfc.submission.exercise.has_user_solved(rfc.user)
         rfc.full_score_reached = true
         rfc.save
       end
