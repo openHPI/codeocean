351fe5f4febac25d563a55ab4fa4bfa563f59bcd
Adrian Steppat <adrian.steppat@student.hpi.de>
2018-03-12 13:15:13 +0100

    Made methods private and fixed empty filename bug (now rejected)

--- lib/proforma/importer.rb
+++ lib/proforma/importer.rb
@@ -25,107 +25,112 @@ module Proforma
       return @exercise
     end
 
-    def add_files_xml(xml)
-      xml.xpath('/p:task/p:files/p:file').each do |file|
-        role = determine_file_role_from_proforma_file(xml, file)
-        filename_attribute = file.xpath('@filename').first
-        file_id = file.xpath('@id').first.value
-        file_class = file.xpath('@class').first.value
-        content = file.text
-        feedback_message = xml.xpath("//p:test/p:test-configuration/p:filerefs/p:fileref[@refid='#{file_id}']/../../c:feedback-message").text
-        @exercise.files.build({
-                        content: content,
-                        name: get_filename_from_filename_attribute(filename_attribute),
-                        path: get_path_from_filename_attribute(filename_attribute),
-                        file_type: get_filetype_from_filename_attribute(filename_attribute),
-                        role: role,
-                        feedback_message: (role == 'teacher_defined_test') ? feedback_message : nil,
-                        hidden: file_class == 'internal',
-                        read_only: false })
-      end
-    end
-
-
-    def split_up_filename(filename)
-      if filename.include? '/'
-        name_with_type = filename.split(/\/(?=[^\/]*$)/).second
-        path = filename.split(/\/(?=[^\/]*$)/).first
-      else
-        name_with_type = filename
-        path = ''
-      end
-      if name_with_type.include? '.'
-        name  = name_with_type.split('.').first
-        type = name_with_type.split('.').second
-      else
-        name = name_with_type
-        type = ''
-      end
-      return path, name, type
-    end
-
-    def get_filetype_from_filename_attribute(filename_attribute)
-
-      if filename_attribute
-        type = split_up_filename(filename_attribute.value).third
-        if type != ''
-          return FileType.find_by(file_extension: ".#{type}")
+    private
+      def add_files_xml(xml)
+        xml.xpath('/p:task/p:files/p:file').each do |file|
+          metadata = get_file_metadata(file)
+          role = determine_file_role_from_proforma_file(xml, metadata)
+          feedback_message = xml.xpath("//p:test/p:test-configuration/p:filerefs/p:fileref[@refid='#{metadata[:file_id]}']/../../c:feedback-message").text
+          filename = get_name_from_filename(metadata[:filename])
+          if filename != ''
+            @exercise.files.build({
+                            content: file.text,
+                            name: filename,
+                            path: get_path_from_filename(metadata[:filename]),
+                            file_type: get_filetype_from_filename(metadata[:filename]),
+                            role: role,
+                            feedback_message: (role == 'teacher_defined_test') ? feedback_message : nil,
+                            hidden: metadata[:file_class] == 'internal',
+                            read_only: false })
+          end
         end
       end
-      return FileType.find_by(name: 'Makefile')
-    end
 
-    def get_filename_from_filename_attribute(filename_attribute)
+      def get_file_metadata(file)
+        file_id = file.xpath('@id').first.value
+        file_class = file.xpath('@class').first.value
+        comment = file.xpath('@comment').first.try(:value)
+        filename = file.xpath('@filename').first
+        {:file_id => file_id, :file_class => file_class, :comment => comment, :filename => filename}
+      end
 
-      if filename_attribute
-        name = split_up_filename(filename_attribute.value).second
-        return name
-      else
+      def split_up_filename(filename)
+        if filename.include? '/'
+          name_with_type = filename.split(/\/(?=[^\/]*$)/).second
+          path = filename.split(/\/(?=[^\/]*$)/).first
+        else
+          name_with_type = filename
+          path = ''
+        end
+        if name_with_type.include? '.'
+          name  = name_with_type.split('.').first
+          type = name_with_type.split('.').second
+        else
+          name = name_with_type
+          type = ''
+        end
+        return path, name, type
+      end
+
+      def get_filetype_from_filename(filename_attribute)
+
+        if filename_attribute
+          type = split_up_filename(filename_attribute.value).third
+          if type != ''
+            return FileType.find_by(file_extension: ".#{type}")
+          end
+        end
+        return FileType.find_by(name: 'Makefile')
+      end
+
+      def get_name_from_filename(filename_attribute)
+
+        if filename_attribute
+          name = split_up_filename(filename_attribute.value).second
+          return name
+        else
+          ''
+        end
+      end
+
+      def get_path_from_filename(filename_attribute)
+
+        if filename_attribute
+          path = split_up_filename(filename_attribute.value).first
+        end
         ''
       end
-    end
 
-    def get_path_from_filename_attribute(filename_attribute)
-
-      if filename_attribute
-        path = split_up_filename(filename_attribute.value).first
+      def determine_file_role_from_proforma_file(xml, metadata)
+        if teacher_defined_test?(xml, metadata)
+          'teacher_defined_test'
+        elsif reference_implementation?(xml, metadata)
+          'reference_implementation'
+        elsif main_file?(metadata)
+          'main_file'
+        elsif no_role?(metadata)
+          ''
+        else
+          'regular_file'
+        end
       end
-      ''
-    end
 
-    def determine_file_role_from_proforma_file(xml, file)
-      file_id = file.xpath('@id').first.value
-      file_class = file.xpath('@class').first.value
-      comment = file.xpath('@comment').first.try(:value)
-      if teacher_defined_test?(xml, file_id, file_class)
-        'teacher_defined_test'
-      elsif reference_implementation?(xml, file_id, file_class)
-        'reference_implementation'
-      elsif (file_class == 'template') && (comment == 'main')
-        'main_file'
-      elsif (file_class == 'internal') && (comment == 'main')
-        ''
-      else
-        'regular_file'
+      def teacher_defined_test?(xml, metadata)
+        is_referenced_by_test = xml.xpath("//p:test/p:test-configuration/p:filerefs/p:fileref[@refid='#{metadata[:file_id]}']")
+        is_referenced_by_test.any? && (metadata[:file_class] == 'internal')
       end
-    end
 
-    def teacher_defined_test?(xml, file_id, file_class)
-      is_referenced_by_test = xml.xpath("//p:test/p:test-configuration/p:filerefs/p:fileref[@refid='#{file_id}']")
-      if is_referenced_by_test.any? && (file_class == 'internal')
-        true
-      else
-        false
+      def reference_implementation?(xml, metadata)
+        is_referenced_by_model_solution = xml.xpath("//p:model-solution/p:filerefs/p:fileref[@refid='#{metadata[:file_id]}']")
+        is_referenced_by_model_solution.any? && (metadata[:file_class] == 'internal')
       end
-    end
 
-    def reference_implementation?(xml, file_id, file_class)
-      is_referenced_by_model_solution = xml.xpath("//p:model-solution/p:filerefs/p:fileref[@refid='#{file_id}']")
-      if is_referenced_by_model_solution.any? && (file_class == 'internal')
-        true
-      else
-        false
+      def main_file?(metadata)
+        (metadata[:file_class] == 'template') && (metadata[:comment] == 'main')
       end
-    end
+
+      def no_role?(metadata)
+        (metadata[:file_class] == 'internal') && (metadata[:comment] == 'main')
+      end
   end
 end
--- lib/proforma/xml_generator.rb
+++ lib/proforma/xml_generator.rb
@@ -10,95 +10,6 @@ module Proforma
       return xml
     end
 
-    def build_proforma_xml_for_head(xml)
-      proforma = xml['p']
-      proforma.description {
-        proforma.cdata(@exercise.description)
-      }
-      execution_environment = @exercise.execution_environment.name.split
-      proforma.proglang(execution_environment.first, 'version' => execution_environment.second)
-      proforma.send('submission-restrictions') {
-        proforma.send('files-restriction') {
-          proforma.send('optional', 'filename' => '')
-        }
-      }
-    end
-
-    def build_proforma_xml_for_single_file(xml, file)
-      if file.role == 'main_file'
-        proforma_file_class = 'template'
-        comment = 'main'
-      else
-        proforma_file_class = 'internal'
-        comment = ''
-      end
-      xml['p'].file(
-          'filename' => file.full_file_name,
-          'id' => file.id,
-          'class' => proforma_file_class,
-          'comment' => comment
-      ) {
-        xml.cdata(file.content)
-      }
-    end
-
-    def build_proforma_xml_for_exercise_files(xml)
-      proforma = xml['p']
-      proforma.files {
-        @exercise.files.all? { |file|
-          build_proforma_xml_for_single_file(xml, file)
-        }
-
-        ### Set Placeholder file for placeholder solution-file and tests if there aren't any
-        if model_solution_files.blank?
-          proforma.file('', 'id' => '0', 'class' => 'internal')
-        end
-      }
-    end
-
-    def build_proforma_xml_for_tests(xml)
-      proforma = xml['p']
-      proforma.tests {
-        tests.each_with_index { |test, index|
-          proforma.test('id' => 't' + index.to_s) {
-            proforma.title('')
-            proforma.send('test-type', 'unittest')
-            proforma.send('test-configuration') {
-              proforma.filerefs {
-                proforma.fileref('refid' => test.id.to_s)
-              }
-              xml['u'].unittest('framework' => testing_framework.first, 'version' => testing_framework.second)
-              xml['c'].send('feedback-message') {
-                xml.cdata(test.feedback_message)
-              }
-            }
-          }
-        }
-      }
-    end
-
-    def build_proforma_xml_for_model_solutions(xml)
-      proforma = xml['p']
-      proforma.send('model-solutions') {
-        if model_solution_files.any?
-          model_solution_files.each_with_index { |model_solution_file, index|
-            proforma = xml['p']
-            proforma.send('model-solution', 'id' => 'm' + index.to_s) {
-              proforma.filerefs {
-                proforma.fileref('refid' => model_solution_file.id.to_s)
-              }
-            }
-          }
-        else ##Placeholder solution_file if there aren't any
-          proforma.send('model-solution', 'id' => 'm0') {
-            proforma.filerefs {
-              proforma.fileref('refid' => '0')
-            }
-          }
-        end
-      }
-    end
-
     def testing_framework
       case @exercise.execution_environment.testing_framework
         when 'RspecAdapter'
@@ -112,24 +23,6 @@ module Proforma
       end
     end
 
-    def to_proforma_xml
-      builder = Nokogiri::XML::Builder.new(:encoding => 'UTF-8') do |xml|
-        proforma = xml['p']
-        proforma.task('xmlns:p' => 'urn:proforma:task:v1.1', 'lang' => 'de', 'uuid' => SecureRandom.uuid,
-                      'xmlns:u' => 'urn:proforma:tests:unittest:v1.1', 'xmlns:c' => 'codeharbor'){
-          build_proforma_xml_for_head(xml)
-          build_proforma_xml_for_exercise_files(xml)
-          build_proforma_xml_for_model_solutions(xml)
-          build_proforma_xml_for_tests(xml)
-          #xml['p'].send('grading-hints', 'max-rating' => @exercise.maxrating.to_s)
-          proforma.send('meta-data') {
-            proforma.title(@exercise.title)
-          }
-        }
-      end
-      return builder.to_xml
-    end
-
     def model_solution_files
       @exercise.files.where(role: 'reference_implementation')
     end
@@ -137,5 +30,113 @@ module Proforma
     def tests
       @exercise.files.where(role: 'teacher_defined_test')
     end
+
+    private
+      def build_proforma_xml_for_head(xml)
+        proforma = xml['p']
+        proforma.description {
+          proforma.cdata(@exercise.description)
+        }
+        execution_environment = @exercise.execution_environment.name.split
+        proforma.proglang(execution_environment.first, 'version' => execution_environment.second)
+        proforma.send('submission-restrictions') {
+          proforma.send('files-restriction') {
+            proforma.send('optional', 'filename' => '')
+          }
+        }
+      end
+
+      def build_proforma_xml_for_single_file(xml, file)
+        if file.role == 'main_file'
+          proforma_file_class = 'template'
+          comment = 'main'
+        else
+          proforma_file_class = 'internal'
+          comment = ''
+        end
+        xml['p'].file(
+            'filename' => file.full_file_name,
+            'id' => file.id,
+            'class' => proforma_file_class,
+            'comment' => comment
+        ) {
+          xml.cdata(file.content)
+        }
+      end
+
+      def build_proforma_xml_for_exercise_files(xml)
+        proforma = xml['p']
+        proforma.files {
+          @exercise.files.all? { |file|
+            build_proforma_xml_for_single_file(xml, file)
+          }
+
+          ### Set Placeholder file for placeholder solution-file and tests if there aren't any
+          if model_solution_files.blank?
+            proforma.file('', 'id' => '0', 'class' => 'internal')
+          end
+        }
+      end
+
+      def build_proforma_xml_for_tests(xml)
+        proforma = xml['p']
+        proforma.tests {
+          tests.each_with_index { |test, index|
+            proforma.test('id' => 't' + index.to_s) {
+              proforma.title('')
+              proforma.send('test-type', 'unittest')
+              proforma.send('test-configuration') {
+                proforma.filerefs {
+                  proforma.fileref('refid' => test.id.to_s)
+                }
+                xml['u'].unittest('framework' => testing_framework.first, 'version' => testing_framework.second)
+                xml['c'].send('feedback-message') {
+                  xml.cdata(test.feedback_message)
+                }
+              }
+            }
+          }
+        }
+      end
+
+      def build_proforma_xml_for_model_solutions(xml)
+        proforma = xml['p']
+        proforma.send('model-solutions') {
+          if model_solution_files.any?
+            model_solution_files.each_with_index { |model_solution_file, index|
+              proforma = xml['p']
+              proforma.send('model-solution', 'id' => 'm' + index.to_s) {
+                proforma.filerefs {
+                  proforma.fileref('refid' => model_solution_file.id.to_s)
+                }
+              }
+            }
+          else ##Placeholder solution_file if there aren't any
+            proforma.send('model-solution', 'id' => 'm0') {
+              proforma.filerefs {
+                proforma.fileref('refid' => '0')
+              }
+            }
+          end
+        }
+      end
+
+      def to_proforma_xml
+        builder = Nokogiri::XML::Builder.new(:encoding => 'UTF-8') do |xml|
+          proforma = xml['p']
+          proforma.task('xmlns:p' => 'urn:proforma:task:v1.1', 'lang' => 'de', 'uuid' => SecureRandom.uuid,
+                        'xmlns:u' => 'urn:proforma:tests:unittest:v1.1', 'xmlns:c' => 'codeharbor'){
+            build_proforma_xml_for_head(xml)
+            build_proforma_xml_for_exercise_files(xml)
+            build_proforma_xml_for_model_solutions(xml)
+            build_proforma_xml_for_tests(xml)
+            #xml['p'].send('grading-hints', 'max-rating' => @exercise.maxrating.to_s)
+            proforma.send('meta-data') {
+              proforma.title(@exercise.title)
+            }
+          }
+        end
+        return builder.to_xml
+      end
   end
 end
