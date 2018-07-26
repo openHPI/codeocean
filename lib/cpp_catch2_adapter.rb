be7eabc5483d65cad26248bcfdfe9ebefb5c5c23
julZuern <41326856+julZuern@users.noreply.github.com>
2018-07-26 11:00:13 +0200

    Update cpp_catch2_adapter.rb

--- lib/cpp_catch2_adapter.rb
+++ lib/cpp_catch2_adapter.rb
@@ -1,19 +1,19 @@
-class CppUnitAdapter < TestingFrameworkAdapter
+class CppCatch2Adapter < TestingFrameworkAdapter
   COUNT_REGEXP 	      = /in (\d+) test cases/ 
   FAILURES_REGEXP     = /test cases: (\d+) | (\d+) failed/
   BOTH_REGEXP		  = /test cases: (\d+) | (\d+) passed | (\d+) failed/
   ASSERTION_ERROR_REGEXP = /\n(.+) error:(.+); /
 
   def self.framework_name
-    'CppUnit'
+    'CppCatch2'
   end
 
   def parse_output(output)
-    if SUCCESS_REGEXP.match(output[:stdout])
+    if COUNT_REGEXP.match(output[:stdout])
       {count: Regexp.last_match(1).to_i, passed: Regexp.last_match(1).to_i}
     else
-      count = COUNT_REGEXP.match(output[:stdout]).try(:captures).try(:first).try(:to_i) || FAILURES_REGEXP.last_match(0).try(:captures).try(:first).try(:to_i) || SUCCESS_REGEXP.last_match(0).try(:captures).try(:first).try(:to_i) || 0
-      failed = FAILURES_REGEXP.match(1).try(:captures).try(:first).try(:to_i) || BOTH_REGEXP.match(2).try(:captures).try(:first).try(:to_i) || 0
+      count = COUNT_REGEXP.match(output[:stdout]).try(:captures).try(:first).try(:to_i) || FAILURES_REGEXP.last_match(1).try(:captures).try(:first).try(:to_i) || BOTH_REGEXP.last_match(1).try(:captures).try(:first).try(:to_i) || 0
+      failed = FAILURES_REGEXP.match(2).try(:captures).try(:first).try(:to_i) || BOTH_REGEXP.match(3).try(:captures).try(:first).try(:to_i) || 0
       error_matches = ASSERTION_ERROR_REGEXP.match(output[:stdout]).try(:captures) || []
       {count: count, failed: failed, error_messages: error_matches}
     end
