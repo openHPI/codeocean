566bc7c4e8a8be54c3be085b6373924465ea5692
Adrian Steppat <adrian.steppat@student.hpi.de>
2018-08-09 13:32:48 +0200

    Fixes codeclimate complaint

--- app/assets/javascripts/codeharbor_link.js
+++ app/assets/javascripts/codeharbor_link.js
@@ -5,6 +5,16 @@ $(function(){
 $(function(){
     if($.isController('code_harbor_links')) {
         if ($('.edit_code_harbor_link, .new_code_harbor_link').isPresent()) {
+
+            var replace = (function(string) {
+                var d = getDate();
+                return string.replace(/[xy]/g, function (c) {
+                    var r = (d + Math.random() * 16) % 16 | 0;
+                    d = Math.floor(d / 16);
+                    return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+                });
+            });
+
             var getDate = (function () {
                 var d = new Date().getTime();
                 if (typeof performance !== 'undefined' && typeof performance.now === 'function') {
@@ -15,21 +25,15 @@ $(function(){
 
             var generateUUID = (function () { // Public Domain/MIT
                 var d = getDate();
-                return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-                    var r = (d + Math.random() * 16) % 16 | 0;
-                    d = Math.floor(d / 16);
-                    return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
-                });
+                return replace('xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx');
             });
 
             var generateRandomHex32 = (function () {
                 var d = getDate();
-                return Array(32).join('x').replace(/[xy]/g, function (c) {
-                    var r = (d + Math.random() * 16) % 16 | 0;
-                    d = Math.floor(d / 16);
-                    return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
-                });
+                return replace(Array(32).join('x'));
             });
+
+
 
             $('.generate-client-id').on('click', function () {
                 $('.client-id').val(generateUUID());
