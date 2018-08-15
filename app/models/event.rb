d528baceadd8270eb09453186d9619017c4ff482
Maximilian Grundke <maximiliangrundke@googlemail.com>
2018-08-15 14:39:58 +0200

    Validate presence of category and data attribute

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,7 @@ class Event < ActiveRecord::Base
   belongs_to :user, polymorphic: true
   belongs_to :exercise
   belongs_to :file
+
+  validates :category, presence: true
+  validates :data, presence: true
 end
