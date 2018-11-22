bb10e191ed711e1f7fb969652f8813457dee9cc0
Sebastian Serth <Sebastian.Serth@student.hpi.de>
2018-11-22 18:57:58 +0100

    Specify `ExternalUser` as class for `user` in `RemoteEvaluationMapping`
    Otherwise, the download of submissions will no longer work in Rails 5

--- app/models/remote_evaluation_mapping.rb
+++ app/models/remote_evaluation_mapping.rb
@@ -2,7 +2,7 @@
 class RemoteEvaluationMapping < ApplicationRecord
   before_create :generate_token, unless: :validation_token?
   belongs_to :exercise
-  belongs_to :user
+  belongs_to :user, class_name: 'ExternalUser'
 
   def generate_token
     self.validation_token = SecureRandom.urlsafe_base64
