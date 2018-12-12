b4b9ab48d05f37445ee7238d07013dc1f0e0070f
Sebastian Serth <Sebastian.Serth@student.hpi.de>
2018-12-12 16:47:49 +0100

    External User: Set name to displayname and introduce real_name

--- app/models/external_user.rb
+++ app/models/external_user.rb
@@ -4,12 +4,28 @@ class ExternalUser < ApplicationRecord
   validates :consumer_id, presence: true
   validates :external_id, presence: true
 
-  def displayname
-    result = name
-    if(result == nil || result == "")
-      result = "User " + id.to_s
-    end
-    result
+  def name
+    # Internal name, shown to teachers and administrators
+    pseudo_name
   end
+
+  def displayname
+    # External name, shown to the user itself and other users, e.g. on RfCs
+    pseudo_name
+  end
+
+  def real_name
+    # Name attribute of the object as persistet in the database
+    self[:name]
+  end
+
+  def pseudo_name
+    if real_name.blank?
+      "User " + id.to_s
+    else
+      real_name
+    end
+  end
+  private :pseudo_name
 
 end
