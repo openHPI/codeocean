83ba0a3f4b84270d5702d748ebee3fcea74178e5
Sebastian Serth <Sebastian.Serth@hpi.de>
2020-03-22 11:07:50 +0100

    Allow new line characters in test results

--- lib/py_unit_adapter.rb
+++ lib/py_unit_adapter.rb
@@ -3,7 +3,7 @@ class PyUnitAdapter < TestingFrameworkAdapter
   FAILURES_REGEXP = /FAILED \(.*failures=(\d+).*\)/
   ERRORS_REGEXP = /FAILED \(.*errors=(\d+).*\)/
   # The regex below also catches new line separators.
-  ASSERTION_ERROR_REGEXP = /AssertionError:\s(.*)\s\s----------------------------------------------------------------------/m
+  ASSERTION_ERROR_REGEXP = /AssertionError:\s(.*?)\s\s----------------------------------------------------------------------/m
 
   def self.framework_name
     'PyUnit'
@@ -15,7 +15,7 @@ class PyUnitAdapter < TestingFrameworkAdapter
     failed = failures_matches ? failures_matches.captures.try(:first).to_i : 0
     error_matches = ERRORS_REGEXP.match(output[:stderr])
     errors = error_matches ? error_matches.captures.try(:first).to_i : 0
-    assertion_error_matches = ASSERTION_ERROR_REGEXP.match(output[:stderr]).try(:captures) || []
+    assertion_error_matches = output[:stderr].scan(ASSERTION_ERROR_REGEXP).flatten || []
     {count: count, failed: failed + errors, error_messages: assertion_error_matches}
   end
 end
