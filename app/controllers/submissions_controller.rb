fcd393b9d5fb08ecff2544c4d49545d7fffa72e7
Sebastian Serth <Sebastian.Serth@hpi.de>
2020-10-08 14:43:22 +0200

    Prevent parsing error for empty client commands

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -184,6 +184,8 @@ class SubmissionsController < ApplicationController
           # Check whether the client send a JSON command and kill container
           # if the command is 'client_kill', send it to docker otherwise.
           begin
+            next if data == "\n"
+
             parsed = JSON.parse(data)
             if parsed.class == Hash && parsed['cmd'] == 'client_kill'
               Rails.logger.debug("Client exited container.")
