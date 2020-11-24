f96da0833b5f36f588cac7e4ef0d0d88d393c173
Sebastian Serth <Sebastian.Serth@hpi.de>
2020-11-24 17:46:19 +0100

    Set RECYCLE_CONTAINERS to false

--- lib/docker_client.rb
+++ lib/docker_client.rb
@@ -9,7 +9,7 @@ class DockerClient
   # Ralf: I suggest to replace this with the environment variable. Ask Hauke why this is not the case!
   LOCAL_WORKSPACE_ROOT = Rails.root.join('tmp', 'files', Rails.env)
   MINIMUM_MEMORY_LIMIT = 4
-  RECYCLE_CONTAINERS = true
+  RECYCLE_CONTAINERS = false
   RETRY_COUNT = 2
   MINIMUM_CONTAINER_LIFETIME = 10.minutes
   MAXIMUM_CONTAINER_LIFETIME = 20.minutes
