f2018044863a931076ae2e5e7e17c9f1e952e395
Sebastian Serth <Sebastian.Serth@hpi.de>
2020-12-01 17:58:33 +0100

    Update webpack config to match newest webpacker gem

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -11,6 +11,11 @@
 #   policy.object_src  :none
 #   policy.script_src  :self, :https
 #   policy.style_src   :self, :https
+#
+#   # You need to allow webpack-dev-server host as allowed origin for connect-src.
+#   # This can be done in Rails 5.2+ for development environment in the CSP initializer
+#   # config/initializers/content_security_policy.rb with a snippet like this:
+#   policy.connect_src :self, :https, "http://localhost:3035", "ws://localhost:3035" if Rails.env.development?
 
 #   # Specify URI for violation reports
 #   # policy.report_uri "/csp-violation-report-endpoint"
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -4,9 +4,9 @@ Info for this file can be found
 github.com/rails/webpacker/blob/master/docs/webpack.md
 */
 
-const { environment } = require('@rails/webpacker');
-const { merge } = require('webpack-merge');
-const webpack = require('webpack');
+const { environment } = require('@rails/webpacker')
+const { merge } = require('webpack-merge')
+const webpack = require('webpack')
 
 // Add an additional plugin of your choosing : ProvidePlugin
 environment.plugins.prepend('Provide', new webpack.ProvidePlugin({
@@ -22,15 +22,15 @@ environment.plugins.prepend('Provide', new webpack.ProvidePlugin({
         Sentry: '@sentry/browser',
         Sortable: 'sortablejs',
     })
-);
+)
 
-const envConfig = module.exports = environment;
+const envConfig = module.exports = environment
 const aliasConfig = module.exports = {
     resolve: {
         alias: {
             jquery: 'jquery/src/jquery',
         }
     }
-};
+}
 
-module.exports = merge(envConfig.toWebpackConfig(), aliasConfig);
+module.exports = merge(envConfig.toWebpackConfig(), aliasConfig)
