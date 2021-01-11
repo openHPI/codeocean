4d270d8fc8bf54bd4b11a5bef5de9d3504a2b0db
Sebastian Serth <Sebastian.Serth@hpi.de>
2021-01-11 17:36:35 +0100

    Cache results for Docker bindings

--- lib/docker_container_mixin.rb
+++ lib/docker_container_mixin.rb
@@ -7,10 +7,15 @@ module DockerContainerMixin
   attr_accessor :docker_client
 
   def binds
-    json['HostConfig']['Binds']
+    host_config['Binds']
   end
 
   def port_bindings
+    # Don't use cached version as this might be changed during runtime
     json['HostConfig']['PortBindings'].try(:map) { |key, value| [key.to_i, value.first['HostPort'].to_i] }.to_h
+  end
+
+  def host_config
+    @host_config ||= json['HostConfig']
   end
 end
