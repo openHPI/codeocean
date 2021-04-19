61e9eab02b4c5fd5b28a968552d331f0f35a1271
Sebastian Serth <Sebastian.Serth@hpi.de>
2021-04-19 18:23:34 +0200

    Load Prometheus if enabled and not in console

--- config/initializers/prometheus.rb
+++ config/initializers/prometheus.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-return unless CodeOcean::Config.new(:code_ocean).read[:prometheus_exporter][:enabled] && defined?(::Rails::Server)
+return unless CodeOcean::Config.new(:code_ocean).read[:prometheus_exporter][:enabled] && !defined?(::Rails::Console)
 return if %w[db: assets:].any? { |task| Rake.application.top_level_tasks.to_s.include?(task) }
 
 # Add metric callbacks to all models
