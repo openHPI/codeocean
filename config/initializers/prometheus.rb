521a0cb710c53fe321134b804967223c0050adff
Sebastian Serth <Sebastian.Serth@hpi.de>
2021-04-19 13:45:21 +0200

    Exclude prometheus task for more rake tasks

--- config/initializers/prometheus.rb
+++ config/initializers/prometheus.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 return unless CodeOcean::Config.new(:code_ocean).read[:prometheus_exporter][:enabled]
-return if Rake.application.top_level_tasks.to_s.include?('db:')
+return if %w[db: assets:].any? { |task| Rake.application.top_level_tasks.to_s.include?(task) }
 
 # Add metric callbacks to all models
 ApplicationRecord.include Prometheus::Record
