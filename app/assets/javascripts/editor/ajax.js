9a5514942eb84967b1ffa3f8ef71e38101507c93
Sebastian Serth <Sebastian.Serth@hpi.de>
2021-04-22 11:31:30 +0200

    Remove CSRF debug features

--- app/assets/javascripts/editor/ajax.js
+++ app/assets/javascripts/editor/ajax.js
@@ -12,11 +12,6 @@ CodeOceanEditorAJAX = {
     $.flash.danger({
       text: message.length > 0 ? message : $('#flash').data('message-failure')
     });
-    Sentry.setContext("error",{
-        cookie: document.cookie,
-        response: response.responseText,
-        csrf: $('meta[name="csrf-token"]').attr("content")
-    });
     Sentry.captureException(JSON.stringify(response));
   }
 };
