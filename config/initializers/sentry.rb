a75328e634c1d005516528fe57d4b48361a45294
Sebastian Serth <Sebastian.Serth@hpi.de>
2021-05-12 17:58:18 +0200

    Enable Sentry Performance

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -3,4 +3,9 @@
 Sentry.init do |config|
   # Do not send full list of gems with each event
   config.send_modules = false
+
+  # Set tracesSampleRate to 1.0 to capture 100%
+  # of transactions for performance monitoring.
+  # We recommend adjusting this value in production
+  config.traces_sample_rate = 1.0
 end
