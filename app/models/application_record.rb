6321b2edefc5f7561c5467f686f4e8d1f912668c
Sebastian Serth <Sebastian.Serth@hpi.de>
2021-10-08 09:26:18 +0200

    trim whitespace from beginning and end of string attributes

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,4 +2,15 @@
 
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  before_validation :strip_strings
+
+  def strip_strings
+    # trim whitespace from beginning and end of string attributes
+    attribute_names.each do |name|
+      if send(name.to_sym).respond_to?(:strip)
+        send("#{name}=".to_sym, send(name).strip)
+      end
+    end
+  end
 end
