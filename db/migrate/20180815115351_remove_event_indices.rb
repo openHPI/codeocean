72501211a618f7a17c79c616fa28deb79bb84fd8
Sebastian Serth <Sebastian.Serth@hpi.de>
2021-11-09 17:38:58 +0100

    Fix issue when migrating an empty database

--- db/migrate/20180815115351_remove_event_indices.rb
+++ db/migrate/20180815115351_remove_event_indices.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
-class RemoveEventIndices < ActiveRecord::Migration[4.2]
+class RemoveEventIndices < ActiveRecord::Migration[6.1]
   def change
-    remove_index :events, %i[user_type user_id]
-    remove_index :events, :exercise_id
-    remove_index :events, :file_id
+    remove_index :events, %i[user_type user_id], if_exists: true
+    remove_index :events, :exercise_id, if_exists: true
+    remove_index :events, :file_id, if_exists: true
   end
 end
