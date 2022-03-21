8b583ea2c96a958dab52bbf213fed413370d3935
Sebastian Serth <Sebastian.Serth@hpi.de>
2022-03-21 09:04:20 +0100

    Fix Rails/DeprecatedActiveModelErrorsMethods

--- app/validators/code_ocean/file_name_validator.rb
+++ app/validators/code_ocean/file_name_validator.rb
@@ -6,7 +6,7 @@ module CodeOcean
       existing_files = File.where(name: record.name, path: record.path, file_type_id: record.file_type_id,
         context_id: record.context_id, context_type: record.context_type).to_a
       if !existing_files.empty? && (!record.context.is_a?(Exercise) || record.context.new_record?)
-        record.errors[:base] << 'Duplicate'
+        record.errors.add(:base, 'Duplicate')
       end
     end
   end
