cdb6c0d64bd94c0ab040d066d1bba15bdff89432
Sebastian Serth <Sebastian.Serth@hpi.de>
2022-06-07 14:29:40 +0200

    Fix Rubocop offense

--- app/models/concerns/default_values.rb
+++ app/models/concerns/default_values.rb
@@ -1,12 +1,10 @@
 # frozen_string_literal: true
 
 module DefaultValues
-  # rubocop:disable Naming/AccessorMethodName
   def set_default_values_if_present(options = {})
     options.each do |attribute, value|
       send(:"#{attribute}=", send(:"#{attribute}") || value) if has_attribute?(attribute)
     end
   end
   private :set_default_values_if_present
-  # rubocop:enable Naming/AccessorMethodName
 end
