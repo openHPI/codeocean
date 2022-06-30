55937d3cc527ce8700d4c95556a2a28a92cbd17a
Sebastian Serth <Sebastian.Serth@hpi.de>
2022-06-30 16:32:09 +0200

    Ensure compatibility with Ruby 3.1

--- spec/helpers/yaml_spec.rb
+++ spec/helpers/yaml_spec.rb
@@ -1,14 +1,23 @@
 # frozen_string_literal: true
 
 require 'find'
-require 'yaml'
+require 'active_support'
+require 'rails'
 
 describe 'yaml config files' do
   Find.find(__dir__, 'config') do |path|
     next unless /.*.\.yml/.match?(path)
 
+    before do
+      allow(Rails).to receive(:root).and_return(Pathname.new('/tmp'))
+
+      app = instance_double Rails::Application
+      allow(Rails).to receive(:application).and_return app
+      allow(app).to receive(:credentials).and_return({})
+    end
+
     it "loads #{path} without syntax error" do
-      expect { YAML.load_file(path) }.not_to raise_error
+      expect { ActiveSupport::ConfigurationFile.parse(path) }.not_to raise_error
     end
   end
 end
