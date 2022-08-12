cc4003baf796741e0a770b5c4a9d6946f204daf2
Sebastian Serth <Sebastian.Serth@hpi.de>
2022-08-12 23:52:23 +0200

    Fix rubocop offenses

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -493,9 +493,9 @@ working_time_accumulated: working_time_accumulated})
 
     if policy(@exercise).detailed_statistics?
       submissions = Submission.where(user: @external_user, exercise: @exercise)
-                              .in_study_group_of(current_user)
-                              .order('created_at')
-      @show_autosaves = params[:show_autosaves] == 'true' || submissions.none? {|s|s.cause != 'autosave'}
+        .in_study_group_of(current_user)
+        .order('created_at')
+      @show_autosaves = params[:show_autosaves] == 'true' || submissions.none? {|s| s.cause != 'autosave' }
       submissions = submissions.where.not(cause: 'autosave') unless @show_autosaves
       interventions = UserExerciseIntervention.where('user_id = ?  AND exercise_id = ?', @external_user.id,
         @exercise.id)
