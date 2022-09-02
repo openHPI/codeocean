51e9daf930ae90df20576e9e00e0b92367d6cb46
Sebastian Serth <Sebastian.Serth@hpi.de>
2022-09-03 00:09:50 +0200

    Enable HSTS preload in app

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -65,6 +65,7 @@ Rails.application.configure do
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
+  config.ssl_options = {hsts: {preload: true}}
 
   # Include generic and useful information about system operation, but avoid logging too much
   # information to avoid inadvertent exposure of personally identifiable information (PII).
