7f0d8b63f97f87f00132390f6388a6be60395d38
Sebastian Serth <Sebastian.Serth@hpi.de>
2022-09-03 10:50:29 +0200

    Use Cookie Prefix in Production and Staging

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,8 +2,19 @@
 
 # Be sure to restart your server when you modify this file.
 
+def self.cookie_prefix
+  if (Rails.env.production? || Rails.env.staging?) \
+    && Rails.application.config.relative_url_root == '/'
+    '__Host-'
+  elsif Rails.env.production? || Rails.env.staging?
+    '__Secure-'
+  else
+    ''
+  end
+end
+
 Rails.application.config.session_store :cookie_store,
-  key: '_code_ocean_session',
+  key: "#{cookie_prefix}CodeOcean-Session",
   expire_after: 1.month,
   secure: Rails.env.production? || Rails.env.staging?,
   path: Rails.application.config.relative_url_root,
