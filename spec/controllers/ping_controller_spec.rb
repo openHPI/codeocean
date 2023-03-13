3f5ef1caf22b711529161efa68c12808491d9314
Sebastian Serth <Sebastian.Serth@hpi.de>
2023-03-13 23:52:59 +0100

    Mock health check for Runner management

--- spec/controllers/ping_controller_spec.rb
+++ spec/controllers/ping_controller_spec.rb
@@ -6,6 +6,8 @@ RSpec.describe PingController do
   render_views
 
   describe 'index' do
+    before { allow(Runner.strategy_class).to receive(:health).and_return(true) }
+
     it 'returns the wanted page and answer with HTTP Status 200' do
       get :index
 
