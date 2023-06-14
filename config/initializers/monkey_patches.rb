d1a0012d4f5f489a1171c0f1cfe5f528a73fa102
Sebastian Serth <Sebastian.Serth@hpi.de>
2023-06-14 14:28:10 +0200

    Overwrite Bootstrap Link Renderer for WillPaginate
    Without this fix, the new will_paginate version is not compatible with the old version of bootstrap-will_paginate. This issue is also tracked here: https://github.com/mislav/will_paginate/issues/649
    
    Fixes CODEOCEAN-QK

--- config/initializers/monkey_patches.rb
+++ config/initializers/monkey_patches.rb
@@ -7,3 +7,13 @@ unless Array.respond_to?(:average)
     end
   end
 end
+
+module WillPaginate
+  module ActionView
+    class Bootstrap4LinkRenderer
+      def previous_or_next_page(page, text, classname, aria_label = nil)
+        tag :li, link(text, page || '#', class: 'page-link', 'aria-label': aria_label), class: [(classname[0..3] if @options[:page_links]), (classname if @options[:page_links]), ('disabled' unless page), 'page-item'].join(' ')
+      end
+    end
+  end
+end
--- spec/features/request_for_comments_filter_spec.rb
+++ spec/features/request_for_comments_filter_spec.rb
@@ -30,4 +30,11 @@ describe 'Request_for_Comments' do
     visit(request_for_comments_path)
     expect(page.find_by_id('q_submission_study_group_id_in')).not_to be_nil
   end
+
+  it 'works with the pagination' do
+    submission = create(:submission)
+    create_list(:rfc, 25, submission:, exercise: submission.exercise)
+    visit(request_for_comments_path)
+    expect(page).to have_css('ul.pagination')
+  end
 end
