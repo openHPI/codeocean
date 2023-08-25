268a9018f28f5b74453976961b667c1cd373f932
kiragrammel <kira.grammel@student.hpi.de>
2023-08-25 16:42:23 +0200

    Allow enabling pair programming study through env

--- lib/pair_programming23_study.rb
+++ lib/pair_programming23_study.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
 class PairProgramming23Study
+  ENABLE = ENV.fetch('PAIR_PROGRAMMING_23_STUDY', nil) == 'true'
+
   def self.participate?
     # TODO: Decide which users are in the study
-    false
+    ENABLE
   end
 end
