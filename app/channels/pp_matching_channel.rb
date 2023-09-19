68c14bde913133741dd0f9388cce314d9a36751b
Sebastian Serth <Sebastian.Serth@hpi.de>
2023-09-19 21:55:39 +0200

    Add authorization for Programming Group matching channel

--- app/channels/pp_matching_channel.rb
+++ app/channels/pp_matching_channel.rb
@@ -2,6 +2,7 @@
 
 class PpMatchingChannel < ApplicationCable::Channel
   def subscribed
+    set_and_authorize_exercise
     stream_from specific_channel
   end
 
@@ -11,6 +12,13 @@ class PpMatchingChannel < ApplicationCable::Channel
   end
 
   def specific_channel
-    "pp_matching_channel_exercise_#{params[:exercise_id]}"
+    "pp_matching_channel_exercise_#{@exercise.id}"
+  end
+
+  private
+
+  def set_and_authorize_exercise
+    @exercise = Exercise.find(params[:exercise_id])
+    reject unless ExercisePolicy.new(current_user, @exercise).implement?
   end
 end
