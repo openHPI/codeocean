7deea1ddac9de08ee1f0713af32c3fe27ba36268
Sebastian Serth <Sebastian.Serth@hpi.de>
2023-09-19 11:17:17 +0200

    Fix invalid timestamps for breadcrumbs in Sentry
    See https://github.com/getsentry/sentry-ruby/issues/1874

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -3,7 +3,7 @@
 Sentry.init do |config|
   config.send_modules = true
   config.include_local_variables = true
-  config.breadcrumbs_logger = %i[sentry_logger monotonic_active_support_logger http_logger]
+  config.breadcrumbs_logger = %i[sentry_logger active_support_logger http_logger]
   config.profiles_sample_rate = 1.0
 
   # Set tracesSampleRate to 1.0 to capture 100%
