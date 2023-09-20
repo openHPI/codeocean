1bd0cd9a566189c04ee0e3a66c2752879e1c9090
Sebastian Serth <Sebastian.Serth@hpi.de>
2023-09-20 23:31:54 +0200

    Require programming group for streaming the editor
    Potentially fixes CODEOCEAN-V2

--- app/policies/programming_group_policy.rb
+++ app/policies/programming_group_policy.rb
@@ -10,6 +10,9 @@ class ProgrammingGroupPolicy < ApplicationPolicy
   end
 
   def stream_sync_editor?
+    # A programming group needs to exist for the user to be able to stream the synchronized editor.
+    return false unless @record
+
     admin? || author_in_programming_group?
   end
 end
