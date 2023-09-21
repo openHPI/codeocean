c6b2505f65857183e9c0dbd82214c7847298f2ea
Sebastian Serth <Sebastian.Serth@hpi.de>
2023-09-21 11:52:18 +0200

    Pass exercise ID as string and refactor channel identifier

--- app/assets/javascripts/channels/pg_matching_channel.js
+++ app/assets/javascripts/channels/pg_matching_channel.js
@@ -3,10 +3,9 @@ $(document).on('turbolinks:load', function () {
     if ($.isController('programming_groups') && window.location.pathname.includes('programming_groups/new')) {
         const matching_page = $('#matching');
         const exercise_id = matching_page.data('exercise-id');
+        const specific_channel = { channel: "PgMatchingChannel", exercise_id: exercise_id};
 
-        App.pg_matching = App.cable.subscriptions.create({
-            channel: "PgMatchingChannel", exercise_id: exercise_id
-        }, {
+        App.pg_matching = App.cable.subscriptions.create(specific_channel, {
             connected() {
                 // Called when the subscription is ready for use on the server
             },
