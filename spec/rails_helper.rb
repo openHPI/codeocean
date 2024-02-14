7356136203ef901008dc1943926d65634012d419
Sebastian Serth <Sebastian.Serth@hpi.de>
2024-02-14 16:05:02 +0100

    Resolve fixture_path deprecation warning in Rails 7.1

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -32,7 +32,7 @@ ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = Rails.root.join('spec/fixtures')
+  config.fixture_paths = [Rails.root.join('spec/fixtures')]
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
