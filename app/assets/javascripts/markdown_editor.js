ec5f74b22cfdccad1843f662eeefcb8726458d92
Julia Casamitjana <62883011+JuliaCasamitjana@users.noreply.github.com>
2024-04-11 10:34:37 +0200

    Install and use ToastUi markdown editor
    Replace all usages of pagedown-bootstrap editor with the new editor.
    Add styles to ensure the editor preview matches the final output.

---
+++ app/assets/javascripts/markdown_editor.js
@@ -0,0 +1,77 @@
+/**
+ * ToastUi editor initializer
+ *
+ * This script transforms form textareas created with 
+ * "PagedownFormBuilder" into ToastUi markdown editors.
+ *
+ */
+
+const initializeMarkdownEditors = () => {
+  const editors = document.querySelectorAll(
+    '[data-behavior="markdown-editor-widget"]'
+  );
+
+  editors.forEach((editor) => {
+    const formInput = document.querySelector(`#${editor.dataset.id}`);
+    if (!editor || !formInput) return;
+
+    const toastEditor = new ToastUi({
+      el: editor,
+      theme: window.getCurrentTheme(),
+      initialValue: formInput.value,
+      placeholder: formInput.placeholder,
+      extendedAutolinks: true,
+      linkAttributes: {
+        target: "_blank",
+      },
+      previewHighlight: false,
+      height: "400px",
+      autofocus: false,
+      usageStatistics: false,
+      language: I18n.locale,
+      toolbarItems: [
+        ["heading", "bold", "italic"],
+        ["link", "quote", "code", "codeblock"],
+        ["ul", "ol"],
+      ],
+      initialEditType: "markdown",
+      events: {
+        change: () => {
+          // Keep real form <textarea> in sync
+          const content = toastEditor.getMarkdown();
+          formInput.value = content;
+        },
+      },
+    });
+
+    // Prevent user from drag'n'dropping images in the editor
+    toastEditor.removeHook("addImageBlobHook");
+
+    // Delegate focus from form input to toast ui editor
+    formInput.addEventListener("focus", () => {
+      toastEditor.focus();
+    });
+  });
+};
+
+const setMarkdownEditorTheme = (theme) => {
+  const editors = document.querySelectorAll(".toastui-editor-defaultUI");
+  editors.forEach((editor) => {
+    const hasDarkTheme = editor.classList.contains("toastui-editor-dark");
+    if (
+      (hasDarkTheme && theme === "light") ||
+      (!hasDarkTheme && theme === "dark")
+    ) {
+      editor.classList.toggle("toastui-editor-dark");
+    }
+  });
+};
+
+$(document).on("turbolinks:load", function () {
+  initializeMarkdownEditors();
+});
+
+$(document).on("theme:change", function (event) {
+  const newTheme = event.detail.currentTheme;
+  setMarkdownEditorTheme(newTheme);
+});
--- app/helpers/pagedown_form_builder.rb
+++ app/helpers/pagedown_form_builder.rb
@@ -1,41 +1,31 @@
 # frozen_string_literal: true
 
 class PagedownFormBuilder < ActionView::Helpers::FormBuilder
-  def pagedown(method, args)
+  def pagedown(method, args = {})
     # Adopt simple form builder to work with form_for
     @attribute_name = method
     @input_html_options = args[:input_html]
 
     @template.capture do
-      @template.concat wmd_button_bar
-      @template.concat wmd_textarea
-      @template.concat wmd_preview if show_wmd_preview?
+      @template.concat form_textarea
+      @template.concat @template.tag.div(class: 'markdown-editor', data: {behavior: 'markdown-editor-widget', id: label_target})
     end
   end
 
   private
 
-  def wmd_button_bar
-    @template.tag.div(nil, id: "wmd-button-bar-#{base_id}")
-  end
-
-  def wmd_textarea
+  def form_textarea
     @template.text_area @object_name, @attribute_name,
-      **@input_html_options,
-      class: 'form-control wmd-input',
-      id: "wmd-input-#{base_id}"
-  end
-
-  def wmd_preview
-    @template.tag.div(nil, class: 'wmd-preview',
-      id: "wmd-preview-#{base_id}")
-  end
-
-  def show_wmd_preview?
-    @input_html_options[:preview].present?
+    **(@input_html_options || {}),
+      id: label_target,
+      class: 'd-none'
   end
 
   def base_id
-    options[:pagedown_id_suffix] || @attribute_name
+    options[:markdown_id_suffix] || @attribute_name
+  end
+
+  def label_target
+    "markdown-input-#{base_id}"
   end
 end
