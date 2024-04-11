9c71c6667a5285d66756eae7a5a94e8ef2615c6b
Julia Casamitjana <62883011+JuliaCasamitjana@users.noreply.github.com>
2024-04-11 10:34:37 +0200

    Install and use ToastUi markdown editor
    Replace all usages of pagedown-bootstrap editor with the new editor.
    Add styles to ensure the editor preview matches the final output.

---
+++ app/javascript/toast-ui.js
@@ -0,0 +1,7 @@
+/* eslint no-console:0 */
+
+// JS
+import ToastUi from "@toast-ui/editor";
+// Import German locales (english ones are included by default)
+import "@toast-ui/editor/dist/i18n/de-de";
+window.ToastUi = ToastUi
