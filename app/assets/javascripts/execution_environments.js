f10bcb96a60e0c2e23b82ffb037c04d55c7db499
Julia Casamitjana <62883011+JuliaCasamitjana@users.noreply.github.com>
2024-04-11 10:55:55 +0200

    Replace Ace Editor with ToastUi editor in the ExecutionEnvironments form
    Drop unused code related to the Ace markdown editor.

--- app/assets/javascripts/execution_environments.js
+++
@@ -1,7 +0,0 @@
-$(document).on('turbolinks:load', function() {
-  if ($.isController('execution_environments')) {
-    if ($('.edit_execution_environment, .new_execution_environment').isPresent()) {
-      new MarkdownEditor('#execution_environment_help');
-    }
-  }
-});
