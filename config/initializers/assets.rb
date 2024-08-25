ac721be5c9be83729c30a543d4c1a8f3409501a2
Sebastian Serth <Sebastian.Serth@hpi.de>
2024-08-25 13:46:02 +0200

    Remove left-over from pagedown-bootstrap
    The necessary change was forgot in 0667cbee

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,7 +3,7 @@
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
-Rails.application.config.assets.version = '1.1'
+Rails.application.config.assets.version = '1.0'
 
 # Add additional assets to the asset load path.
 # We require the config directory to enable asset dependencies on CodeOcean::Config values (stored as YML files in `config`).
@@ -12,4 +12,4 @@ Rails.application.config.assets.paths << Rails.root.join('config')
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-Rails.application.config.assets.precompile += %w[markdown-buttons.png]
+# Rails.application.config.assets.precompile += %w[ admin.js admin.css ]
