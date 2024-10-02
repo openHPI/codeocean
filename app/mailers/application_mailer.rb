8d0d91a7854f6163136ac6641ed7b85b88a7acf8
Sebastian Serth <Sebastian.Serth@hpi.de>
2024-10-02 04:18:23 +0200

    Remove non-working default for ApplicationMailer
    The `default` setting within the ApplicationMailer is overwriting the previously-set values from `config/action_mailer.yml`. Of course, that is not intended and needs to be fixed.

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,8 +1,5 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  include ApplicationHelper
-
-  default from: "#{application_name} <codeocean@openhpi.de>"
   layout 'mailer'
 end
