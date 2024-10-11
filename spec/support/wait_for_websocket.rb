5ac1bb8e132125e4ec8a4dadf8c3fe2c990bca78
Sebastian Serth <Sebastian.Serth@hpi.de>
2024-10-11 23:39:43 +0200

    Selenium: Refactor WaitFor* scripts to prevent crashes
    The previously used approach with `Timeout.timeout` works. However, it might interrupt the `page.evaluate_script`, which will cause an unhandled exception with the Selenium webdriver which in turn crashes. Once crashed, all further Selenium-based tests will fail, too.

--- spec/support/wait_for_websocket.rb
+++ spec/support/wait_for_websocket.rb
@@ -2,12 +2,19 @@
 
 module WaitForWebsocket
   def wait_for_websocket
-    Timeout.timeout(Capybara.default_max_wait_time) do
-      loop until websocket_finished?
+    start_time = Time.current
+    timeout = Capybara.default_max_wait_time
+
+    loop do
+      break if websocket_finished? || (Time.current - start_time) > timeout
+
+      sleep 0.1 # Short sleep time to prevent busy waiting
     end
   end
 
   def websocket_finished?
+    # This method MUST NOT be interrupted. Hence, Timeout.timeout is not used here.
+    # Otherwise, Selenium and the browser driver might crash, preventing further tests from running.
     page.evaluate_script('CodeOceanEditorWebsocket?.websocket?.getReadyState() === WebSocket.CLOSED').present?
   end
 end
