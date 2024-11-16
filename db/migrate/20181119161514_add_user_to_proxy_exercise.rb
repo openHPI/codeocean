35c601ced8656a550c8223532ba3e15f0d43ad48
Sebastian Serth <Sebastian.Serth@hpi.de>
2024-11-16 12:08:56 +0100

    Fix incorrect class name in backfill migration

--- db/migrate/20181119161514_add_user_to_proxy_exercise.rb
+++ db/migrate/20181119161514_add_user_to_proxy_exercise.rb
@@ -13,6 +13,6 @@ class AddUserToProxyExercise < ActiveRecord::Migration[5.2]
     add_column :proxy_exercises, :public, :boolean, null: false, default: false
 
     internal_user = InternalUser.find_by(id: 46) || InternalUser.first
-    ProxyExercise.update(user_id: internal_user&.id || 1, user_type: internal_user.class.name)
+    ProxyExercise.update(user_id: internal_user&.id || 1, user_type: internal_user.class.name.demodulize)
   end
 end
