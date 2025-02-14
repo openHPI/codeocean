3ade0861cd6095ddda3f1535406fc4bbf3d00f7e
Sebastian Serth <Sebastian.Serth@hpi.de>
2025-02-14 17:08:38 +0100

    Disable Runner Management for FactoryBot linting

--- spec/other/factories_spec.rb
+++ spec/other/factories_spec.rb
@@ -3,6 +3,14 @@
 require 'rails_helper'
 
 RSpec.describe 'Factories' do
+  let(:codeocean_config) { instance_double(CodeOcean::Config) }
+  let(:runner_management_config) { {runner_management: {enabled: false}} }
+
+  before do
+    allow(CodeOcean::Config).to receive(:new).with(:code_ocean).and_return(codeocean_config)
+    allow(codeocean_config).to receive(:read).and_return(runner_management_config)
+  end
+
   it 'are all valid', permitted_execution_time: 30 do
     expect { FactoryBot.lint }.not_to raise_error
   end
