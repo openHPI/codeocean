3c548fd0323845d70e24d28e5b8f0f1515e8caa9
Armin Kirchner <post.armin@gmail.com>
2025-06-05 16:51:45 +0200

    authorize!

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -2,12 +2,16 @@
 
 class ReportsController < ApplicationController
   def create
-    authorize(reported_content, policy_class: ReportPolicy)
+    authorize!
     ReportMailer.with(reported_content:).report_content.deliver_later
     redirect_back(fallback_location: :root, notice: t('reports.reported'))
   end
 
   private
+
+  def authorize!
+    authorize(reported_content, policy_class: ReportPolicy)
+  end
 
   def reported_content
     @reported_content ||= GlobalID::Locator.locate(params.require(:global_content_id))
