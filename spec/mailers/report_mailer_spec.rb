c3e5d75d4596747828e6098b41de94fa1ceb06bb
Armin Kirchner <post.armin@gmail.com>
2025-06-13 15:01:37 +0200

    Move erb to slim and add html

--- spec/mailers/report_mailer_spec.rb
+++ spec/mailers/report_mailer_spec.rb
@@ -6,21 +6,20 @@ RSpec.describe ReportMailer do
   describe '#report_content' do
     subject(:mail) { described_class.with(reported_content:).report_content }
 
-    let(:reported_content) { create(:comment, text: 'Inappropriate content for Comment.') }
+    let(:reported_content) { create(:rfc, question: 'Inappropriate content for RfC.') }
 
     it 'sets the correct sender' do
       expect(mail.from).to include('codeocean@openhpi.de')
     end
 
     context 'when a RfC is reported' do
-      let(:reported_content) { create(:rfc, question: 'Inappropriate content for RfC.') }
-
       it 'sets the correct subject' do
         expect(mail.subject).to eq(I18n.t('report_mailer.report_content.subject', content_name: RequestForComment.model_name.human))
       end
 
       it 'includes the reported content' do
-        expect(mail.body).to include('Inappropriate content for RfC.')
+        expect(mail.text_part.body).to include('Inappropriate content for RfC.')
+        expect(mail.html_part.body).to include('Inappropriate content for RfC.')
       end
     end
   end
