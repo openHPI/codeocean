2ea1db4c00850a7fde3c0a3c28f29e99242bd1ce
Armin Kirchner <post.armin@gmail.com>
2025-06-17 12:32:44 +0200

    redirect with explicit path

--- app/controllers/request_for_comments_controller.rb
+++ app/controllers/request_for_comments_controller.rb
@@ -168,7 +168,7 @@ class RequestForCommentsController < ApplicationController
 
     ReportMailer.with(reported_content: @request_for_comment).report_content.deliver_later
 
-    redirect_to(@request_for_comment, notice: t('.report.reported'))
+    redirect_to(request_for_comment_path(@request_for_comment), notice: t('.report.reported'))
   end
 
   private
