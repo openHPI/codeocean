831bfa27b762147e0fac57a6c6b7bc61cbf297d8
Armin Kirchner <post.armin@gmail.com>
2025-06-17 14:13:08 +0200

    remove request test authorization helper

--- spec/support/authentication.rb
+++ spec/support/authentication.rb
@@ -6,13 +6,6 @@ module Authentication
   end
 end
 
-module RequestLoginHelper
-  def login_as(user)
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-  end
-end
-
 RSpec.configure do |config|
   config.include Authentication, type: :system
-  config.include RequestLoginHelper, type: :request
 end
