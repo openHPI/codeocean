9c0419b79c0daece36e3a77d408bebb725ae8028
Armin Kirchner <post.armin@gmail.com>
2025-06-17 12:24:50 +0200

    stub authorization in request test

--- spec/request/request_for_comments_spec.rb
+++ spec/request/request_for_comments_spec.rb
@@ -4,6 +4,7 @@ require 'rails_helper'
 
 RSpec.describe 'POST /request_for_comments/:rfc_id/report', type: :request do
   before do
+    login_as(create(:learner))
     codeocean_config = instance_double(CodeOcean::Config)
     allow(CodeOcean::Config).to receive(:new).with(:code_ocean).and_return(codeocean_config)
     allow(codeocean_config).to receive(:read).and_return({
@@ -12,9 +13,6 @@ RSpec.describe 'POST /request_for_comments/:rfc_id/report', type: :request do
   end
 
   it 'sends an email to let admins know about the report' do
-    user = create(:learner)
-    password = attributes_for(:learner).fetch(:password)
-    login_as(user, password)
     rfc = create(:rfc)
 
     expect { post(report_request_for_comment_path(rfc)) }
--- spec/support/authentication.rb
+++ spec/support/authentication.rb
@@ -7,8 +7,8 @@ module Authentication
 end
 
 module RequestLoginHelper
-  def login_as(user, password)
-    post(sessions_url, params: {email: user.email, password:})
+  def login_as(user)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
   end
 end
 
