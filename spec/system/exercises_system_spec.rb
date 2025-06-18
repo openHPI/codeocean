7fd86f9f4b34e1df71cb33241d9d24b39ad3c2f9
Armin Kirchner <post.armin@gmail.com>
2025-06-18 17:12:38 +0200

    test: Add system tests for exercise form

--- spec/system/exercises_system_spec.rb
+++ spec/system/exercises_system_spec.rb
@@ -3,18 +3,21 @@
 require 'rails_helper'
 
 RSpec.describe 'Exercise', :js do
+  let(:teacher) { create(:teacher) }
+
   before do
-    teacher = create(:teacher)
     visit(sign_in_path)
     fill_in('email', with: teacher.email)
     fill_in('password', with: attributes_for(:teacher)[:password])
-    binding.irb
     click_button(I18n.t('sessions.new.link'))
   end
 
-  it 'foo' do
+  it 'creates an exercise' do
     visit new_exercise_path
 
-    binding.irb
+    fill_in :exercise_title, with: 'xxx'
+
+    # TODO: - set execution enviorment
+    #       - add errors to exercises form
   end
 end
