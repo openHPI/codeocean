031d7d1c9784322c729de062c25d8913c4b5020c
Armin Kirchner <post.armin@gmail.com>
2025-06-20 11:49:36 +0200

    fix: move mailer test let statements into context

--- spec/mailers/report_mailer_spec.rb
+++ spec/mailers/report_mailer_spec.rb
@@ -6,14 +6,14 @@ RSpec.describe ReportMailer do
   describe '#report_content' do
     subject(:mail) { described_class.with(reported_content:).report_content }
 
-    let(:question) { 'Inappropriate content for RfC.' }
-    let(:reported_content) { create(:rfc, question:) }
-
-    it 'sets the correct sender' do
-      expect(mail.from).to include('codeocean@openhpi.de')
-    end
-
     context 'when an RfC is reported' do
+      let(:question) { 'Inappropriate content for RfC.' }
+      let(:reported_content) { create(:rfc, question:) }
+
+      it 'sets the correct sender' do
+        expect(mail.from).to include('codeocean@openhpi.de')
+      end
+
       it 'sets the correct subject' do
         expect(mail.subject).to eq(I18n.t('report_mailer.report_content.subject', content_name: RequestForComment.model_name.human))
       end
