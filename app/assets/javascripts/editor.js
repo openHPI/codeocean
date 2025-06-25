7576d1e16b5eef39dd502535c8a7b486c442718d
Sebastian Serth <Sebastian.Serth@hpi.de>
2025-06-26 01:22:38 +0200

    Prevent duplication of editor content
    By removing the content once added to the ACE editor, we prevent an issue with Turbolinks / Turbo duplicating the lines. This issue became apparent during the migration to Turbo, since its behavior changed in comparison to Turbolinks.
    
    See https://github.com/hotwired/turbo/issues/1413

--- app/assets/javascripts/editor.js
+++ app/assets/javascripts/editor.js
@@ -14,9 +14,6 @@ $(document).on('turbolinks:load', function(event) {
   );
 
   if ($('#editor').isPresent() && CodeOceanEditor && event.originalEvent.data.url.includes("/implement")) {
-    // This call will (amon other things) initializeEditors and load the content except for the last line
-    // It must not be called during page navigation. Otherwise, content will be duplicated!
-    // Search for insertFullLines and Turbolinks reload / cache control
     CodeOceanEditor.initializeEverything();
   }
 
