5f6b43d9134d21ca42ea6531e9dbb2849a434f41
Sebastian Serth <Sebastian.Serth@hpi.de>
2025-07-01 22:31:24 +0200

    Install event listener only once

--- app/assets/javascripts/editor.js
+++ app/assets/javascripts/editor.js
@@ -16,13 +16,13 @@ $(document).on('turbo-migration:load', function(event) {
   if ($('#editor').isPresent() && CodeOceanEditor && event.detail.url.includes("/implement")) {
     CodeOceanEditor.initializeEverything();
   }
-
-  function handleThemeChangeEvent(event) {
-    if (CodeOceanEditor) {
-      CodeOceanEditor.THEME = event.detail.currentTheme === 'dark' ? 'ace/theme/tomorrow_night' : 'ace/theme/tomorrow';
-      document.dispatchEvent(new Event('theme:change:ace'));
-    }
-  }
-
-  $(document).on('theme:change', handleThemeChangeEvent.bind(this));
 });
+
+function handleThemeChangeEvent(event) {
+  if (CodeOceanEditor) {
+    CodeOceanEditor.THEME = event.detail.currentTheme === 'dark' ? 'ace/theme/tomorrow_night' : 'ace/theme/tomorrow';
+    document.dispatchEvent(new Event('theme:change:ace'));
+  }
+}
+
+$(document).on('theme:change', handleThemeChangeEvent);
