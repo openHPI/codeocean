4e62ffba4060a5a9a8263ba991c7cce4a1c70e66
Sebastian Serth <Sebastian.Serth@hpi.de>
2025-07-02 10:34:34 +0200

    JS Performance: Unload JsTree on navigation
    Similar to the ACE editor, we unload the JsTree to prevent memory leaks. Also, we use this opportunity to reduce code duplication.

--- app/assets/javascripts/submission_statistics.js
+++ app/assets/javascripts/submission_statistics.js
@@ -46,11 +46,7 @@ $(document).on('turbo-migration:load', function(event) {
         });
         showActiveFile();
       });
-      $(document).on('theme:change', function(event) {
-          const newColorScheme = event.detail.currentTheme;
-          // Update the JStree theme
-          fileTree.jstree(true).set_theme(newColorScheme === "dark" ? "default-dark" : "default");
-      });
+      CodeOceanEditor.installFileTreeEventHandlers(fileTree);
       fileTrees.push(fileTree);
     });
   };
