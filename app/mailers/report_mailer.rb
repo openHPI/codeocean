e283689a54c6e7fc1bc0c69b5b3e622bb8a445aa
Armin Kirchner <post.armin@gmail.com>
2025-07-11 14:47:35 +0200

    Comment about varying URL formats.

--- app/mailers/report_mailer.rb
+++ app/mailers/report_mailer.rb
@@ -7,6 +7,10 @@ class ReportMailer < ApplicationMailer
     @reported_content = params.fetch(:reported_content)
     study_group = @reported_content.submission.study_group
     exercise = @reported_content.exercise
+
+    # NOTE: This implementation assumes the curse URL is static and does not vary per user.
+    # This is currently valid for all known use cases. However, in dynamic environments—such as
+    # advanced content trees in openHPI or A/B/n testing—this assumption may break.
     @course_url = LtiParameter.find_by(study_group:, exercise:)&.lti_parameters&.[]('launch_presentation_return_url')
 
     mail(subject: I18n.t('report_mailer.report_content.subject', content_name: @reported_content.model_name.human))
